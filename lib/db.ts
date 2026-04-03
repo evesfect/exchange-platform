573ae20c31d17bd305488b9430e11c7d21f777c8
evesfect <evesfect@gmail.com>
2026-04-03 15:07:14 +0300

    drizzle setup

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,11 @@
-// src/lib/db.ts
-import { Pool } from 'pg';
+// lib/db.ts
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "./schema";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-export default pool;
+// Export the Drizzle database instance
+export const db = drizzle(pool, { schema });
